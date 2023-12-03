3bafcab9cf8a74760607fa1bd9e1521273bab963
Vlad0244 <suprun@sheridancollege.ca>
2023-12-02 21:55:53 -0500

    first commit

---
+++ downloadFromLinks.py
@@ -0,0 +1,5 @@
+'''
+Download songs from json file using a YT API based on links. 
+Maybe implement running duplicateSearchAndCleanUp.py on the json file to 
+remove any duplicates. 
+'''
---
+++ workingWithYTplaylist/createJsonFromYtPlaylist.py
@@ -0,0 +1,11 @@
+from youtubesearchpython import Playlist
+import sys
+def jsonFromYtPlaylist(playlist):
+    yt_playlist = Playlist(playlist)
+    prints = 0
+    while yt_playlist.hasMoreVideos:
+        yt_playlist.getNextVideos()
+        print(f'Videos Retrieved: {len(yt_playlist.videos)}')
+        # add code to create json file from playlist
+    
+jsonFromYtPlaylist('https://www.youtube.com/playlist?list=PLia5vPs9sr0s6c2SDvCU8D_8BzDMh4N4B')
