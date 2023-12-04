4a02de44390f4a45336f15d4b50559b58f4432bb
Vlad0244 <suprun@sheridancollege.ca>
2023-12-04 01:10:49 -0500

    test

--- workingWithYTplaylist/createJsonFromYtPlaylist.py
+++ workingWithYTplaylist/createJsonFromYtPlaylist.py
@@ -7,5 +7,5 @@ def jsonFromYtPlaylist(playlist):
         yt_playlist.getNextVideos()
         print(f'Videos Retrieved: {len(yt_playlist.videos)}')
         # add code to create json file from playlist
-    
+    print(yt_playlist)
 jsonFromYtPlaylist('https://www.youtube.com/playlist?list=PLia5vPs9sr0s6c2SDvCU8D_8BzDMh4N4B')
